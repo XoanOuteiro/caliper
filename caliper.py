c97ab30ede7c65c06fb22bd7f98fc4fdbaa690ed
XoanOuteiro <115654591+XoanOuteiro@users.noreply.github.com>
2025-03-14 18:55:56 +0100

    Added logo

--- caliper.py
+++ caliper.py
@@ -10,8 +10,10 @@ def run():
     args = Argparser()
 
 if __name__ == "__main__":
-    Utilities.print_separator("*", 30)
+
+    Utilities.print_separator("<>", 30)
+    print(Utilities.logo + "\n\t --- " + Utilities.VERSION + " --- by @XoanOuteiro")
     print(Utilities.get_random_quote())
-    Utilities.print_separator("*", 30)
+    Utilities.print_separator("<>", 30)
 
     run()
