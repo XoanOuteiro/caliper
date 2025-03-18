aedf6e0786ae0466148d3d9674676eda7393ece6
XoanOuteiro <xoanouteirojorge@gmail.com>
2025-03-18 15:55:38 +0100

    Added base EVAL module structure

---
+++ modules/evaluator.py
@@ -0,0 +1,27 @@
+'''
+-- Caliper
+-- author/s: @XoanOuteiro
+'''
+
+'''
+Conducts the EVAL mode to discover potentially useful
+non-blocked content
+'''
+class Evaluator:
+
+    '''
+    --- Instance Attributes ---
+    '''
+    url : str = None
+    parameter : str = None
+
+
+    '''
+    --- Constructors ---
+    '''
+    def __init__(self, url : str, paramenter : str):
+
+
+    '''
+    --- Instance Methods ---
+    '''
